202cf7b7a7b6616eee9c89f2c9068fee9915ca10
rilindaxhaqkaj <rilinda@centralsoft.io>
2021-12-12 15:55:11 +0100

    implemented-custom-button

---
+++ src/components/button/CustomButton.vue
@@ -0,0 +1,44 @@
+<template>
+<button class="Button" 
+:style ="{backgroundColor:bgColor, borderRadius:borderRadius, borderColor:borderColor,width:width, height:height}">
+    <i v-bind:class="startIcon"></i>
+    {{title}}
+</button>
+</template>
+
+<script>
+export default {
+  props: {
+    title:{
+        type:String,
+    },
+    bgColor:{
+        type:String,
+        default:"#41b3ff"
+    },
+    borderRadius:{
+        type:String,
+        default:"50px"
+    },
+    borderColor:{
+        type:String,
+        default:"#41b3ff"
+    },
+    startIcon:{
+        type: Element
+    },
+    width:{
+        type:String,
+        default:"100%"
+    },
+     height:{
+        type:String,
+        default:"40px"
+    }
+  }
+}
+</script>
+
+<style>
+@import "./CustomButton.styles.scss";
+</style>
