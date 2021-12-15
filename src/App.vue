ef3103d854298f2828a50b55ec23d465fefe9d0f
agnesa <agnesademolli01@gmail.com>
2021-12-15 22:19:55 +0100

    custom-inputField

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Header />
   <Footer />
+  <InputField/>
 </template>
 
 <script>
 import Footer from "./components/footer";
 import Header from "./components/header";
+import InputField from './components/CustomInputField/InputField';
 
 export default {
   name: "App",
   components: {
     Footer,
     Header,
+    InputField
   },
 };
 </script>
---
+++ src/components/CustomInputField/InputField.vue
@@ -0,0 +1,138 @@
+<template>
+  <div
+    class="InputField"
+    :class="{ 'has-error': !!errorMessage, success: meta.valid }"
+  >
+    <label :for="name">{{ label }}</label>
+    <input
+      :name="name"
+      :id="name"
+      :type="type"
+      :value="inputValue"
+      :placeholder="placeholder"
+      @input="handleChange"
+      @blur="handleBlur"
+    />
+
+    <p class="help-message" v-show="errorMessage || meta.valid">
+      {{ errorMessage || successMessage }}
+    </p>
+  </div>
+</template>
+
+<script>
+
+import { useField } from "vee-validate";
+
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    successMessage: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props) {
+ n
+    const {
+      value: inputValue,
+      errorMessage,
+      handleBlur,
+      handleChange,
+      meta,
+    } = useField(props.name, undefined, {
+      initialValue: props.value,
+    });
+
+    return {
+      handleChange,
+      handleBlur,
+      errorMessage,
+      inputValue,
+      meta,
+    };
+  },
+};
+</script>
+
+<style  scoped>
+.InputField {
+  position: relative;
+  margin-bottom: calc(1em * 1.5);
+  width: 100%;
+}
+
+label {
+  display: block;
+  margin-bottom: 4px;
+  width: 100%;
+}
+
+input {
+  border-radius: 5px;
+  border: 2px solid transparent;
+  padding: 15px 10px;
+  outline: none;
+  background-color: #f2f5f7;
+  width: 100%;
+  transition: border-color 0.3s ease-in-out, color 0.3s ease-in-out,
+    background-color 0.3s ease-in-out;
+}
+
+input:focus {
+  border-color: var(--primary-color);
+}
+
+.help-message {
+  position: absolute;
+  bottom: calc(-1.5 * 1em);
+  left: 0;
+  margin: 0;
+  font-size: 14px;
+}
+
+.InputField.has-error input {
+  background-color: var(--error-bg-color);
+  color: var(--error-color);
+}
+
+.TextIInputFieldnput.has-error input:focus {
+  border-color: var(--error-color);
+}
+
+.InputField.has-error .help-message {
+  color: var(--error-color);
+}
+
+.InputField.success input {
+  background-color: var(--success-bg-color);
+  color: var(--success-color);
+}
+
+.InputField.success input:focus {
+  border-color: var(--success-color);
+}
+
+.InputField.success .help-message {
+  color: var(--success-color);
+}
+</style>
