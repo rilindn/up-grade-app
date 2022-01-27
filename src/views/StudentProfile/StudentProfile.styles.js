98051aaf9a731ae912f729f9d0cd2fc27e5d9682
leonita27 <leonitafetahu27@gmail.com>
2022-01-27 00:32:43 -0800

    add layout of profile page

--- src/views/StudentProfile/StudentProfile.styles.js
+++ src/views/StudentProfile/StudentProfile.styles.js
@@ -2,8 +2,40 @@ import styled from "vue3-styled-components";
 import palette from "../../theme/palette";
 
 export const Wrapper = styled.div`
+height: 740px;
+display: flex;
   width: 100%;
   text-align: center;
+  flex-direction:column;
+`;
+
+export const TopWrapper = styled.div`
+height: 300px;
+  width: 100%;
+  background-color: ${palette.green[700]};
+`;
+
+export const MiddleBox = styled.div`
+height: auto;
+  width: '100%';
+  flex-direction:row;
+  margin-top:-35px
+  display: flex;
+  justify-content: space-around;
+`;
+export const LeftWrapper = styled.div`
+height: 400px;
+width:350px;
+box-shadow: 3px 6px 5px 0px rgba(176,176,176,0.75);
+
+  background-color: ${palette.common['white']};
+`;
+
+export const RightWrapper = styled.div`
+height: 400px;
+width:900px;
+box-shadow: 3px 6px 5px 0px rgba(176,176,176,0.75);
+background-color: ${palette.common['white']};
 `;
 
 
--- src/views/StudentProfile/StudentProfile.vue
+++ src/views/StudentProfile/StudentProfile.vue
@@ -1,13 +1,27 @@
 <template>
   <Wrapper>
+    <TopWrapper>
     <p>THIS ISSS PROFILE PAGE</p>
+    </TopWrapper>
+    <MiddleBox>
+     <RightWrapper>
+    <p>THIS ISSS PROFILE PAGE</p>
+    </RightWrapper>
+         <LeftWrapper>
+    <p>THIS ISSS PROFILE PAGE</p>
+    </LeftWrapper>
+    </MiddleBox>
   </Wrapper>
 </template>
 <script>
-import { Wrapper, } from "./StudentProfile.styles";
+import { Wrapper,TopWrapper,MiddleBox,RightWrapper ,LeftWrapper} from "./StudentProfile.styles";
 export default {
   components: {
     Wrapper,
+    TopWrapper,
+    MiddleBox,
+    RightWrapper,
+    LeftWrapper
   
   },
 };
