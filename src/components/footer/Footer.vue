ac4965ca61fa3aed4dfa5ab10db9adcb37d86093
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-01-29 22:32:16 +0100

    feat: redesign-footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,52 +1,14 @@
 <template>
   <Wrapper>
-    <FirstWrapper>
-      <h4>FOLLOW US</h4>
-      <FirstMiddleWrapper>
-      <span><i class="fab fa-facebook-f"></i>Facebook</span>
-      <span><i class="fab fa-twitter"></i>Twitter</span>
-      <span><i class="fab fa-linkedin-in"></i>Linked In</span>
-     <span> <i class="fab fa-instagram"></i>Instagram</span>
-      </FirstMiddleWrapper>
-    </FirstWrapper>
-    <FirstWrapper>
-      <h4> GETTING ABOUT</h4>
-       <List>
-      <span>Home</span>
-      <span>Join</span>
-      <span>Contact Us</span>
-      <span>List Stuff</span>
-      <span>Help</span>
-      </List>
-    </FirstWrapper>
-    <FirstWrapper>
-     <h4>IMPORTANT STUFF</h4> 
-      <List>
-      <span>About E-Learning</span>
-      <span>Privacy Policy</span>
-      <span>Terms of Use</span>
-      <span>Stuff you can't list</span>
-      </List>
-    </FirstWrapper>
-    <FirstWrapper>
-      <h4>HELPFUL TIPS</h4>
-      <List>
-      <span>Etiquette</span>
-      <span>E-Learning Policy</span>
-      <span>E-Learning Glossary</span>
-      </List>
-    </FirstWrapper>
+    <span> Copyright <i class="far fa-copyright"></i> 2022 <b> Up Grade </b>. All Rights Reserved. </span>
   </Wrapper>
 </template>
 
 <script>
-import { Wrapper, FirstWrapper,FirstMiddleWrapper, List} from "./Footer.styles";
+import { Wrapper} from "./Footer.styles";
 export default {
   components: {
     Wrapper,
-    FirstWrapper,
-    FirstMiddleWrapper,
-    List,
   },
 };
 </script>
