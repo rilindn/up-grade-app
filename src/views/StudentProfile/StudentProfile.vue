d29fae1953f108f731eb06c45ca5c5153d12c2e8
leonita27 <leonitafetahu27@gmail.com>
2022-02-03 20:09:55 -0800

    fixes

--- src/views/StudentProfile/StudentProfile.vue
+++ src/views/StudentProfile/StudentProfile.vue
@@ -7,23 +7,23 @@
           <img src="@/assets/profile.jpeg" width="140"  alt="logo" />
        </PhotoWrapper>
          <NameWrapper>
-        <div><div></div><p>{{ $t("studentProfile.name") }}</p><span>{{ $t("studentProfile.active") }}</span></div>
+        <div><div></div><p>Maria Ahshhs</p><span>(Active Student)</span></div>
       </NameWrapper>
        </TopWrapper>
        <Container>
            <FirstContainer>
              <div>
-                 <div><p>{{ $t("studentProfile.id") }}:</p><span>{{ $t("studentProfile.idNr") }}</span></div>
-              <div><p>{{ $t("studentProfile.birthday") }}:</p><span>{{ $t("studentProfile.date") }}</span></div>
-              <div><p>{{ $t("studentProfile.nationality") }}:</p><span>{{ $t("studentProfile.nationalityPlace") }}</span></div>
-                 <div><p>{{ $t("studentProfile.citizenship") }}:</p><span>{{ $t("studentProfile.citizenshipPlace") }}</span></div>
-               <div><p>{{ $t("studentProfile.gender") }}:</p><span>{{ $t("studentProfile.genderType") }}</span></div>
+                 <div><p>{{ $t("studentProfile.id") }}:</p><span>19038128</span></div>
+              <div><p>{{ $t("studentProfile.birthday") }}:</p><span>1 July  2000 (age 21)</span></div>
+              <div><p>{{ $t("studentProfile.nationality") }}:</p><span>United States</span></div>
+                 <div><p>{{ $t("studentProfile.citizenship") }}:</p><span>United States</span></div>
+               <div><p>{{ $t("studentProfile.gender") }}:</p><span>Female</span></div>
             </div>
            </FirstContainer>
             <SecondContainer>
              <div>
-            <div><p>{{ $t("studentProfile.place") }}:</p><span>{{ $t("studentProfile.placeName") }}</span></div>
-           <div><p>{{ $t("studentProfile.zipcode") }}:</p><span>{{ $t("studentProfile.zipcodeNumber") }}</span></div>
+            <div><p>{{ $t("studentProfile.place") }}:</p><span>New York,USA</span></div>
+           <div><p>{{ $t("studentProfile.zipcode") }}:</p><span>172520</span></div>
              </div>
             </SecondContainer>
       </Container>
@@ -32,11 +32,11 @@
         <div>
          <h1>Contact Info</h1>
          <div> <i class="far fa-envelope color"  style="color:#d7d7d7"></i><p>{{ $t("studentProfile.personalEmail") }}</p></div>
-         <span>{{ $t("studentProfile.personalEmailText") }}</span>
+         <span>marias@gmail.com</span>
          <div><i class="fas fa-phone-square color" style="color:#d7d7d7"></i><p>{{ $t("studentProfile.phoneNumber") }}</p></div>
-         <span>{{ $t("studentProfile.phoneNumberNr") }}</span>
+         <span>+323239939393</span>
          <div> <i class="far fa-envelope color"  style="color:#d7d7d7"></i> <p>{{ $t("studentProfile.studentEmail") }}</p></div>
-         <span>{{ $t("studentProfile.studentEmailText") }}</span>
+         <span>maria@ubt-uni.com</span>
         </div>
      </LeftWrapper>
     </MiddleBox>
