068ed2a1f0e973507c9a91a0abc4a77ca5b25d9d
rilind nuha <rn48707@ubt-uni.net>
2022-02-08 23:45:36 +0100

    feat: added select box component

--- src/views/Register/Register.vue
+++ src/views/Register/Register.vue
@@ -5,14 +5,12 @@
       <InputField name="" placeholder="Firstname" />
       <InputField name="" placeholder="Lastname" />
       <InputField name="" type="email" placeholder="Email" />
-      <va-select
-        class="role-select"
+      <SelectInput
+        :error="errors"
+        name="role"
         placeholder="Choose a role"
-        v-model="role"
-        color="#008466"
         :options="['Student', 'Staff']"
-      >
-      </va-select>
+      />
       <DateInput :error="errors" name="dateOfBirth" />
       <Button
         :title="'Register'"
@@ -30,6 +28,7 @@ import { Wrapper, FormWrapper } from "./Register.styles";
 import Button from "@/components/button";
 import DateInput from "@/components/DateInput";
 
+import SelectInput from "@/components/SelectInput";
 export default {
   components: {
     InputField,
@@ -37,6 +36,7 @@ export default {
     FormWrapper,
     Button,
     DateInput,
+    SelectInput,
   },
   data() {
     return {
@@ -52,6 +52,10 @@ export default {
 }
 .va-input__content-wrapper {
   height: 50px;
+  margin: 15px;
+  border-radius: 5px;
+}
+.va-input__content-wrapper {
   padding: 13px 10px;
 }
 .va-input__container {
