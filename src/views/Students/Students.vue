f7d3849dcf3cd42641bccd933459580e9a115b27
rilind nuha <rn48707@ubt-uni.net>
2022-02-20 01:49:29 +0100

    feat: add pagination

--- src/views/Students/Students.vue
+++ src/views/Students/Students.vue
@@ -5,40 +5,47 @@
         <span><i class="fas fa-plus-circle"></i></span>
         <span>Add New</span>
       </AddNew>
-      <Table>
-        <Head>
-          <Column>Student ID</Column>
-          <Column>Name</Column>
-          <Column>Lastname</Column>
-          <Column>Email</Column>
-          <Column>Actions</Column>
-        </Head>
-        <Body>
-          <Row v-for="(user, i) in users" :key="user.id" :index="++i">
-            <Cell>{{ user.studentId }}</Cell>
-            <Cell>{{ user.firstName }}</Cell>
-            <Cell>{{ user.lastName }}</Cell>
-            <Cell>{{ user.email }}</Cell>
-            <Cell>
-              <ActionWrapper>
-                <Edit @click="editModal(user)"
-                  ><i class="far fa-edit"></i
-                ></Edit>
-                <Delete @click="handleDelete(user._id)">
-                  <i class="far fa-trash-alt"></i
-                ></Delete>
-              </ActionWrapper>
-            </Cell>
-          </Row>
-        </Body>
-      </Table>
+      <va-inner-loading :loading="users?.length <= 0" :size="60">
+        <Table>
+          <Head>
+            <Column>Student ID</Column>
+            <Column>Name</Column>
+            <Column>Lastname</Column>
+            <Column>Email</Column>
+            <Column>Actions</Column>
+          </Head>
+          <Body>
+            <Row v-for="(user, i) in users" :key="user.id" :index="++i">
+              <Cell>{{ user.studentId }}</Cell>
+              <Cell>{{ user.firstName }}</Cell>
+              <Cell>{{ user.lastName }}</Cell>
+              <Cell>{{ user.email }}</Cell>
+              <Cell>
+                <ActionWrapper>
+                  <Edit @click="editModal(user)"
+                    ><i class="far fa-edit"></i
+                  ></Edit>
+                  <Delete @click="handleDelete(user._id)">
+                    <i class="far fa-trash-alt"></i
+                  ></Delete>
+                </ActionWrapper>
+              </Cell>
+            </Row>
+          </Body>
+        </Table>
+        <Paginator
+          @fetchPaginationItems="fetchPaginationItems"
+          :page="page"
+          :pager="pager"
+        />
+      </va-inner-loading>
     </Wrapper>
     <va-modal v-model="showModal" hide-default-actions>
       <slot>
         <EditStudent
           :data="editUserData"
           @closeModal="closeModal"
-          @fetchStudents="fetchStudents"
+          @refetchStudents="fetchStudentsOnAction"
         />
       </slot>
     </va-modal>
@@ -57,6 +64,8 @@ import {
 } from "./Students.styles";
 import EditStudent from "./EditStudent";
 import { getAllStudents, deleteStudent } from "@/api/ApiMethods";
+import { paginationStudents } from "../../api/ApiMethods";
+import Paginator from "@/components/Paginator";
 export default {
   components: {
     Table,
@@ -72,12 +81,15 @@ export default {
     AddNew,
     Container,
     EditStudent,
+    Paginator,
   },
   data() {
     return {
       users: [],
+      pager: {},
       showModal: false,
       editUserData: [],
+      currentPage: 1,
     };
   },
   methods: {
@@ -88,9 +100,8 @@ export default {
     closeModal() {
       this.showModal = false;
     },
-    async fetchStudents() {
-      const users = await getAllStudents();
-      this.users = users;
+    async fetchStudentsOnAction() {
+      await this.fetchPaginationItems(this.currentPage);
     },
     async handleDelete(id) {
       if (confirm("Are you sure?")) {
@@ -100,13 +111,20 @@ export default {
           duration: 2000,
           text: "User deleted!",
         });
-        await this.fetchStudents();
+        await this.fetchStudentsOnAction();
       }
+    },
+    async fetchPaginationItems(page) {
+      this.currentPage = page;
+      const data = await paginationStudents(page);
+      this.users = data.pageOfItems;
+      this.pager = data.pager;
     },
   },
   async beforeCreate() {
-    const users = await getAllStudents();
-    this.users = users;
+    const data = await paginationStudents(1);
+    this.users = data.pageOfItems;
+    this.pager = data.pager;
   },
 };
 </script>
