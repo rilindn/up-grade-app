3d25c7a4ee02d1d52fc8ed526fb77b7df2dfde21
rilind nuha <rn48707@ubt-uni.net>
2022-03-13 01:53:30 +0100

    feat: schedule for student

--- src/views/Schedule/Schedule.vue
+++ src/views/Schedule/Schedule.vue
@@ -3,91 +3,64 @@
     <SecondWrapper>
       <Container>
         <WeekWrapper>
-          <div>
-            <span>Mon</span>
-          </div>
-          <div>
-            <span>Tue</span>
-          </div>
-          <div>
-            <span>Wed</span>
-          </div>
-          <div>
-            <span>Thur</span>
-          </div>
-          <div>
-            <span>Fri</span>
-          </div>
+          <WeekDay
+            v-for="(day, i) in days"
+            :key="i"
+            @click="handleSelectedDay(i, day.full)"
+            :isSelected="selectedDayOrder === i"
+          >
+            <span>{{ day.short }}</span>
+          </WeekDay>
         </WeekWrapper>
-        <Course>
+        <Course
+          v-for="(hour, i) in schedule?.days?.[this.selectedDayOrder]?.hours"
+          :key="hour?._id"
+          @click="handleSelectedHour(i, hour)"
+          :isSelected="selectedHourOrder === i"
+        >
           <CourseContainer>
-            <p>1</p>
-            <CourseCode>
-              <p>Mathematics</p>
-              <p>MAT001</p>
-            </CourseCode>
-          </CourseContainer>
-          <CourseContainer>
-            <p>2</p>
-            <CourseCode>
-              <p>Biology</p>
-              <p>BIO002</p>
-            </CourseCode>
-          </CourseContainer>
-          <CourseContainer>
-            <p>3</p>
-            <CourseCode>
-              <p>English</p>
-              <p>ENG003</p>
-            </CourseCode>
-          </CourseContainer>
-          <CourseContainer>
-            <p>4</p>
-            <CourseCode>
-              <p>Mathematics</p>
-              <p>MAT001</p>
-            </CourseCode>
-          </CourseContainer>
-          <CourseContainer>
-            <p>5</p>
-            <CourseCode>
-              <p>Biology</p>
-              <p>BIO002</p>
-            </CourseCode>
-          </CourseContainer>
-          <CourseContainer>
-            <p>6</p>
-            <CourseCode>
-              <p>English</p>
-              <p>ENG003</p>
-            </CourseCode>
+            <span>{{ i + 1 }}</span>
+            <span>{{ hour?.course }}</span>
+            <span>{{ hour?.courseCode }}</span>
           </CourseContainer>
         </Course>
       </Container>
+      <ThirdWrapper>
+        <ul>
+          <ListItem>
+            <span>Course:</span><span>{{ selectedHour?.course || "---" }}</span>
+          </ListItem>
+          <ListItem>
+            <span>Class:</span
+            ><span>{{ selectedHour?.classroom || "---" }}</span>
+          </ListItem>
+          <ListItem>
+            <span>Code:</span
+            ><span>{{ selectedHour?.courseCode || "---" }}</span>
+          </ListItem>
+          <ListItem>
+            <span>Professor:</span
+            ><span>{{ selectedHour?.teacher || "---" }}</span>
+          </ListItem>
+          <ListItem>
+            <span>Start time:</span
+            ><span>{{
+              selectedHour?.startTime
+                ? moment(selectedHour?.startTime).format("hh:mm A")
+                : "---"
+            }}</span>
+          </ListItem>
+          <ListItem>
+            <span>End time:</span
+            ><span>{{
+              selectedHour?.startTime
+                ? moment(selectedHour?.endTime).format("hh:mm A")
+                : "---"
+            }}</span>
+          </ListItem>
+        </ul>
+      </ThirdWrapper>
     </SecondWrapper>
-    <ThirdWrapper>
-      <ul>
-        <ListItem>
-          <span>{{ $t("schedule.course") }}:</span><span>Mathematics</span>
-        </ListItem>
-        <ListItem>
-          <span>{{ $t("schedule.class") }}:</span><span>Z008</span>
-        </ListItem>
-        <ListItem>
-          <span>{{ $t("schedule.code") }}:</span><span>MAT001</span>
-        </ListItem>
-        <ListItem>
-          <span>{{ $t("schedule.professor") }}:</span
-          ><span>Rilinda Xhaqkaj</span>
-        </ListItem>
-        <ListItem>
-          <span>{{ $t("schedule.startTime") }}:</span><span>08:00 AM</span>
-        </ListItem>
-        <ListItem>
-          <span>{{ $t("schedule.endTime") }}:</span><span>08:45 AM</span>
-        </ListItem>
-      </ul>
-    </ThirdWrapper>
   </Wrapper>
 </template>
 <script>
@@ -101,7 +74,11 @@ import {
   CourseContainer,
   CourseCode,
   ListItem,
+  AddHour,
+  WeekDay,
 } from "./Schedule.styles";
+import days from "@/data/days";
+import { getStudentSchedule } from "@/api/ApiMethods";
 export default {
   components: {
     Wrapper,
@@ -113,6 +90,39 @@ export default {
     CourseContainer,
     CourseCode,
     ListItem,
+    AddHour,
+    WeekDay,
+  },
+  data() {
+    return {
+      days,
+      schedule: {},
+      selectedDay: "Monday",
+      selectedDayOrder: 0,
+      selectedHour: {},
+      selectedHourOrder: 0,
+    };
+  },
+  methods: {
+    async getSchedule() {
+      const studentId = this.$store.getters.loggedUser?.id;
+      const schedule = await getStudentSchedule(studentId);
+      this.schedule = schedule;
+      this.selectedHour = schedule?.days?.[this.selectedDayOrder]?.hours?.[0];
+    },
+    handleSelectedHour(order, hour) {
+      this.selectedHour = hour;
+      this.selectedHourOrder = order;
+    },
+    handleSelectedDay(order, val) {
+      this.selectedDay = val;
+      this.selectedDayOrder = order;
+      this.selectedHourOrder = 0;
+      this.selectedHour = this.schedule?.days?.[order]?.hours?.[0];
+    },
+  },
+  async created() {
+    await this.getSchedule();
   },
 };
 </script>
